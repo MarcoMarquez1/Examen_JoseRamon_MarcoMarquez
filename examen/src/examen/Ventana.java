abb5324e304740499d8bef9a8e35f3552bab7027
MarcoMarquez1 <127341565+MarcoMarquez1@users.noreply.github.com>
2023-03-27 15:15:02 -0600

    Segundo Avance
    Avance

--- examen/src/examen/Ventana.java
+++ examen/src/examen/Ventana.java
@@ -3,6 +3,7 @@ package examen;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -59,6 +61,14 @@ public class Ventana extends JFrame {
 		if(actual.equals("registro")) { 
 			gran_panel = registro();  
 		}
+		if(actual.equals("comoCrear")) { 
+			gran_panel = comoCrear();  
+		}
+		if(actual.equals("cuentapersonal")) { 
+			gran_panel = cuentapersonal();  
+		}
+		
+		
 
 		this.add(gran_panel);
 		this.revalidate();
@@ -182,78 +192,61 @@ public class Ventana extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				anterior = actual;
-				actual = "principal";
+				String email = mail.getText();
 
-				route();
+				char[] pwChrs = pwd.getPassword();
+				String password = new String(pwChrs);
 
+				BufferedReader reader;
+
+				try {
+					reader = new BufferedReader(new FileReader("usuarios.txt"));
+
+					String line = reader.readLine();
+
+					while (line != null) {
+
+						String[] datos = null;
+
+						datos = line.split(",");
+
+						System.out.println(datos[2]);
+
+						if(datos[2].equals(email)) {
+							if(datos[3].equals(password)) {
+								
+								anterior = actual;
+								actual = "principal";
+								route();
+								
+								JOptionPane.showMessageDialog(null, "Acceso correcto");
+								break;
+							}
+						}
+						else {
+							JOptionPane.showMessageDialog(null, "Acceso incorrecto");
+							break;
+						}
+					}
+					
+
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
 			}
 
 		});
+				
+
+			
 
 		return login;
 	}
-	
-	//------------------------------Panel de principal -------------------------------------------
-			public JPanel principal() {
-
-				JPanel principal = new JPanel();
-				principal.setVisible(true);
-				principal.setSize(600, 600);
-				principal.setBackground(Color.CYAN);
-				principal.setLayout(null);
-				
-				JLabel tag8 = new JLabel("HOLA, BIENVENIDO", JLabel.CENTER);
-				tag8.setFont(new Font("Comic Sans",Font.BOLD,23));
-				tag8.setSize(400, 20);
-				tag8.setLocation(100, 100);
-				tag8.setOpaque(true);
-				tag8.setBackground(Color.white);
-				principal.add(tag8);
-				
-				ImageIcon imagen =new ImageIcon("Foto 3.png");
-				JButton im1=new JButton(imagen);
-				im1.setSize(170,170);
-				im1.setLocation(210,250);
-				
-				Image esc = imagen.getImage().getScaledInstance(im1.getWidth(), im1.getHeight(),Image.SCALE_SMOOTH);
-				Icon ices=new ImageIcon(esc);
-				im1.setIcon(ices);
-				principal.add(im1);
-				
-				JMenuBar jmb = new JMenuBar();
-				jmb.setSize(this.getWidth(),20);
-				
-				this.add(jmb);
-				
-				JMenu jm1 = new JMenu("CUENTA");
-				JMenu jm2 = new JMenu("ARCHIVO");
-				JMenu jm3 = new JMenu("AYUDA");
-				jmb.add(jm1);
-				jmb.add(jm2);
-				jmb.add(jm3);
-				
-				JMenuItem jmi1 = new JMenuItem("LISTA DE USUARIOS ");
-				JMenuItem jmi2 = new JMenuItem("CREAR USUARIOS ");
-				
-				jm2.add(jmi1);
-				jm2.add(jmi2);
-				
-				
-				JMenuItem jmi3 = new JMenuItem("MI CUENTA ");
-				JMenuItem jmi4 = new JMenuItem("CERRAR SESION ");
-				
-				jm1.add(jmi3);
-				jm1.add(jmi4);
-				
-				
-
-				JMenuItem jmi5 = new JMenuItem("¿COMO CREAR USUARIOS? ");
-				jm3.add(jmi5);
-
-				return principal;
-			}
-
 	//------------------------------Panel de registro -------------------------------------------
 	public JPanel registro() {
 
@@ -338,7 +331,340 @@ public class Ventana extends JFrame {
 			}
 
 		});
-		return registro;
-
+		return registro;		
 	}
+
+	//-----------------------PANEL PARA VER COMO CREAR USUARIOS------------------------------------
+	public JPanel comoCrear () {
+
+		JPanel comoCrear = new JPanel();
+		comoCrear.setVisible(true);
+		comoCrear.setSize(600, 600);
+		comoCrear.setBackground(Color.CYAN);
+		comoCrear.setLayout(null);
+		
+		JLabel tag1 = new JLabel("¿COMO CREAR UN USUARIO?", JLabel.CENTER);
+		tag1.setFont(new Font("Comic Sans",Font.BOLD,23));
+		tag1.setSize(380, 20);
+		tag1.setLocation(100, 50);
+		tag1.setOpaque(true);
+		tag1.setBackground(Color.white);
+		comoCrear.add(tag1);
+		
+		ImageIcon imagen =new ImageIcon("Foto 6.png");
+		JButton im6=new JButton(imagen);
+		im6.setSize(120,100);
+		im6.setLocation(220,100);
+		
+		Image esc = imagen.getImage().getScaledInstance(im6.getWidth(), im6.getHeight(),Image.SCALE_SMOOTH);
+		Icon ices=new ImageIcon(esc);
+		im6.setIcon(ices);
+		comoCrear.add(im6);
+		
+		
+		
+		JLabel tag2 = new JLabel("1- haga click en la opcion usuarios en el menu superior ");
+		tag2.setFont(new Font("arial",Font.BOLD,15));
+		tag2.setSize(600,20);
+		tag2.setLocation(50,270);
+		comoCrear.add(tag2);	
+	
+		JLabel tag3 = new JLabel("2- Hacer click en la opcion Crear");
+		tag3.setFont(new Font("arial",Font.BOLD,15));
+		tag3.setSize(600,30);
+		tag3.setLocation(50,290);
+		comoCrear.add(tag3);
+			
+			
+		JLabel tag4 = new JLabel("3- Llenar los campos solicitados");
+		tag4.setFont(new Font("arial",Font.BOLD,15));
+		tag4.setSize(600,30);
+		tag4.setLocation(50,310);
+		comoCrear.add(tag4);
+			
+		JLabel tag5 = new JLabel("4- Escribe Una Pequeña Desripcion De Ti ");
+		tag5.setFont(new Font("arial",Font.BOLD,15));
+		tag5.setSize(600,20);
+		tag5.setLocation(50,335);
+		comoCrear.add(tag5);
+			
+			
+		JLabel tag6 = new JLabel("5- Selecciona Tu Comida Favorita");
+		tag6.setFont(new Font("arial",Font.BOLD,15));
+		tag6.setSize(600,30);
+		tag6.setLocation(50,350);
+		comoCrear.add(tag6);
+	
+		JLabel tag7 = new JLabel("6- Selecciona Tu Color Favorito");
+		tag7.setFont(new Font("arial",Font.BOLD,15));
+		tag7.setSize(600,30);
+		tag7.setLocation(50,370);
+		comoCrear.add(tag7);
+						
+		JLabel tag8 = new JLabel("7- Hacer Click En El Boton Crear Usuario");
+		tag8.setFont(new Font("arial",Font.BOLD,15));
+		tag8.setSize(600,30);
+		tag8.setLocation(50,390);
+		comoCrear.add(tag8);
+			
+		JLabel tag9 = new JLabel("8- Listo El Usuario Se Ha Creado");
+		tag9.setFont(new Font("arial",Font.BOLD,15));
+		tag9.setSize(600,30);
+		tag9.setLocation(50,410);
+		comoCrear.add(tag9);
+		
+		JButton irCrear = new JButton("Ir a crear");
+		irCrear.setSize(380, 35);
+		irCrear.setLocation(100,500);
+		irCrear.setBackground(Color.white);
+		comoCrear.add(irCrear);
+		
+		
+		/*irCrear.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+		
+				anterior = actual;
+				actual = "login";
+				
+				route();
+			}
+			
+		});*/
+			
+		return comoCrear;
+	}
+	
+	
+//----------------PANEL PARA VER TU CUENTA-----------------------------------------	
+	
+	public JPanel cuentapersonal () {
+		
+		JPanel cuentapersonal = new JPanel();
+		cuentapersonal.setVisible(true);
+		cuentapersonal.setSize(600, 600);
+		cuentapersonal.setBackground(Color.CYAN);
+		cuentapersonal.setLayout(null);
+	
+		
+		JLabel tag1 = new JLabel("MI CUENTA PERSONAL", JLabel.CENTER);
+		tag1.setFont(new Font("Comic Sans",Font.BOLD,23));
+		tag1.setSize(300, 30);
+		tag1.setLocation(110, 20);
+		tag1.setOpaque(true);
+		tag1.setBackground(Color.white);
+		cuentapersonal.add(tag1);
+		
+		
+		ImageIcon imagen =new ImageIcon("Foto 4.png");
+		JButton im4=new JButton(imagen);
+		im4.setSize(100,100);
+		im4.setLocation(220,55);
+		Image esc = imagen.getImage().getScaledInstance(im4.getWidth(), im4.getHeight(),Image.SCALE_SMOOTH);
+		Icon ices=new ImageIcon(esc);
+		im4.setIcon(ices);
+		cuentapersonal.add(im4);
+		
+		JLabel tag2 = new JLabel("Ingrese su nombre: ");
+		tag2.setSize(200, 100);
+		tag2.setLocation(190, 120);
+		tag2.setFont(new Font("Arial", Font.BOLD, 15));
+		tag2.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(tag2);
+		
+		
+		JTextField nombre = new JTextField();
+		nombre.setSize(380, 20);
+		nombre.setLocation(100, 180);
+		cuentapersonal.add(nombre);
+		
+		JLabel tag3 = new JLabel("Ingrese su apellidos: ");
+		tag3.setSize(200, 100);
+		tag3.setLocation(190, 160);
+		tag3.setFont(new Font("Arial", Font.BOLD, 15));
+		tag3.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(tag3);
+		
+		JTextField apellido = new JTextField();
+		apellido.setSize(380,20);
+		apellido.setLocation(100,220);
+		apellido.setFont(new Font("comic sans",Font.ITALIC,14));
+		cuentapersonal.add(apellido);
+		
+		
+		JLabel tag4 = new JLabel("Ingrese correo electrónico:");
+		tag4.setSize(200,20);
+		tag4.setFont(new Font("Arial", Font.BOLD, 15));
+		tag4.setLocation(190, 240);
+		tag4.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(tag4);
+		
+		
+		JTextField mail_reg = new JTextField();
+		mail_reg.setSize(380, 20);
+		mail_reg.setLocation(100, 260);
+		mail_reg.setFont(new Font("Comic Sans",Font.ITALIC,15));
+		cuentapersonal.add(mail_reg);
+		
+
+		JLabel tag5 = new JLabel("Ingrese su contraseña: ");
+		tag5.setSize(200, 100);
+		tag5.setLocation(190, 240);
+		tag5.setFont(new Font("Arial", Font.BOLD, 15));
+		tag5.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(tag5);
+		
+		JPasswordField pwd_reg = new JPasswordField();
+		pwd_reg.setSize(380, 20);
+		pwd_reg.setLocation(100, 300);
+		cuentapersonal.add(pwd_reg);
+		
+		JLabel tag6 = new JLabel("Ingrese descripcion");
+		tag6.setSize(200, 20);
+		tag6.setLocation(190, 325);
+		tag6.setFont(new Font("Arial", Font.BOLD, 15));
+		tag6.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(tag6);
+		
+		TextArea texto = new TextArea();
+		texto.setSize(380, 50);
+		texto.setLocation(100, 350);
+		texto.setFont(new Font("Arial", Font.BOLD, 15));
+		texto.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(texto);
+		
+		JLabel tag7 = new JLabel("Comida Favorita");
+		tag7.setSize(200, 20);
+		tag7.setLocation(200, 400);
+		tag7.setFont(new Font("Arial", Font.BOLD, 15));
+		tag7.setForeground(Color.decode("#000000"));
+		cuentapersonal.add(tag7);
+		
+		JCheckBox mi_check = new JCheckBox();
+		mi_check.setText("pizza");
+		mi_check.setSize(100, 15);
+		mi_check.setLocation(100, 420);
+		mi_check.setOpaque(true);
+		mi_check.setForeground(Color.BLACK);
+		this.add(mi_check);
+		
+		JCheckBox mi_check2 = new JCheckBox();
+		mi_check2.setText("Sushi");
+		mi_check2.setSize(100, 15);
+		mi_check2.setLocation(220, 420);
+		mi_check2.setOpaque(true);
+		mi_check2.setForeground(Color.BLACK);
+		this.add(mi_check2);
+		
+		JCheckBox mi_check3 = new JCheckBox();
+		mi_check3.setText("hamburguesa");
+		mi_check3.setSize(100, 15);
+		mi_check3.setLocation(340, 420);
+		mi_check3.setOpaque(true);
+		mi_check3.setForeground(Color.BLACK);
+		this.add(mi_check3);
+		
+		/*JButton saveUsr = new JButton("C A N C E L A R");
+		saveUsr.setSize(200, 35);
+		saveUsr.setLocation(60,480);
+		saveUsr.setBackground(Color.white);
+		cuentapersonal.add(saveUsr);
+		
+		
+		JButton saveUsr1 = new JButton("A C T U A L I Z A R");
+		saveUsr1.setSize(200, 35);
+		saveUsr1.setLocation(300,480);
+		saveUsr1.setBackground(Color.white);
+		cuentapersonal.add(saveUsr1);*/
+	
+		return cuentapersonal;
+	}
+	//------------------------------Panel de principal -------------------------------------------
+	public JPanel principal() {
+
+		JPanel principal = new JPanel();
+		principal.setVisible(true);
+		principal.setSize(600, 600);
+		principal.setBackground(Color.CYAN);
+		principal.setLayout(null);
+		
+		JLabel tag8 = new JLabel("HOLA, BIENVENIDO", JLabel.CENTER);
+		tag8.setFont(new Font("Comic Sans",Font.BOLD,23));
+		tag8.setSize(400, 20);
+		tag8.setLocation(100, 100);
+		tag8.setOpaque(true);
+		tag8.setBackground(Color.white);
+		principal.add(tag8);
+		
+		ImageIcon imagen =new ImageIcon("Foto 3.png");
+		JButton im1=new JButton(imagen);
+		im1.setSize(170,170);
+		im1.setLocation(210,250);
+		
+		Image esc = imagen.getImage().getScaledInstance(im1.getWidth(), im1.getHeight(),Image.SCALE_SMOOTH);
+		Icon ices=new ImageIcon(esc);
+		im1.setIcon(ices);
+		principal.add(im1);
+		
+		JMenuBar jmb = new JMenuBar();
+		jmb.setSize(this.getWidth(),20);
+		
+		this.add(jmb);
+		
+		JMenu jm1 = new JMenu("CUENTA");
+		JMenu jm2 = new JMenu("ARCHIVO");
+		JMenu jm3 = new JMenu("AYUDA");
+		jmb.add(jm1);
+		jmb.add(jm2);
+		jmb.add(jm3);
+		
+		JMenuItem jmi1 = new JMenuItem("LISTA DE USUARIOS ");
+		JMenuItem jmi2 = new JMenuItem("CREAR USUARIOS ");
+		
+		jm2.add(jmi1);
+		jm2.add(jmi2);
+		
+		
+		JMenuItem jmi3 = new JMenuItem("MI CUENTA ");
+		JMenuItem jmi4 = new JMenuItem("CERRAR SESION ");
+		
+		jm1.add(jmi3);
+		jm1.add(jmi4);
+		
+		
+		
+		jmi3.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				remove(principal);
+				add(cuentapersonal());
+				repaint();
+			}
+			
+		});
+		/*JMenuItem jmi5 = new JMenuItem("¿COMO CREAR USUARIOS? ");
+		jm3.add(jmi5);
+		
+		jmi5.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			remove(principal);
+			add(comoCrear());
+			
+			repaint();
+		}
+		
+	});*/
+
+		return principal;
+	}
+
+	
+	
 }
